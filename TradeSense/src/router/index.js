dc9afaccac42d891f8cfc3ef3a07a532ad5f069b
shu <Email1>
2025-10-03 17:45:56 +0900

    init5

--- TradeSense/src/router/index.js
+++ TradeSense/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import TradeAdvice from '../views/Dashboard.vue'
 import QuantStrategy from '../views/QuantStrategy.vue'
@@ -29,7 +29,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHashHistory('/TradeSense/'),
-  routes
+  routes,
 })
 
 export default router
